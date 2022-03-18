e606d861abbedb033dd8ac1d53f9b98092550ac1
Shums Kassam <skassam21@gmail.com>
2022-03-17 22:07:09 -0400

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -11,7 +11,7 @@ router.post('/register', async (req, res, next) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-      return res.status(400).json({ error: 'username and password required fields' });
+      return res.status(400).json({ error: 'username and password required' });
     }
 
     if (password.length < 6) {
@@ -52,7 +52,7 @@ router.post('/login', async (req, res, next) => {
   try {
     const { username, password } = req.body;
     if (!username || !password) {
-      return res.status(400).json({ error: 'username and password required' });
+      return res.status(400).json({ error: 'username and password required fields' });
     }
 
     const author = await Author.findOne({
